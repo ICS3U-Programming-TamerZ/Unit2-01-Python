8cdeae07cc65c9cd8899ac23075dbfac6d357277
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2022-09-22 22:33:13 +0000

    Added area_per_circle.py

---
+++ area_per_circle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Tamer Zreg
+# Created on: Sep/22/2022
+# This program calculates and displays the area and perimeter of a
+# circle with radius 30 mm.
+import math
+
+
+def main():
+    print("For a circle that has a radius")
+    print("of 30 mm:")
+    print("")
+    print("Area = {} mm^2". format(math.pi*30**2))
+    print("Perimeter = {} mm". format(2*math.pi*30))
+
+
+if __name__ == "__main__":
+    main()
